b92761aece5de7ee9c105931869d48866ddbd6cc
Anthony <jieanthony75@gmail.com>
2024-07-19 11:49:26 +0800

    优化

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,23 +4,21 @@ use App\Http\Response;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        // web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        // health: '/up',
         then: function () {
             Route::redirect('/', '/api');
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
 
+        // 全局中间件
         $middleware->use([
             // \Illuminate\Http\Middleware\TrustHosts::class,
             // \Illuminate\Http\Middleware\TrustProxies::class,
@@ -31,9 +29,12 @@ return Application::configure(basePath: dirname(__DIR__))
             // \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         ]);
 
-        $middleware->api(remove: [
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ]);
+        $middleware->api(
+            remove: [
+                \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            ]
+        );
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
 
@@ -42,9 +43,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Exceptions\BusinessException::class,
         ]);
 
-        $response = app(Response::class);
+        // 未知的异常统一渲染
+        $exceptions->render(function (\Exception $exception) {
+            $response = app(Response::class);
 
-        $exceptions->render(function (\Exception $exception) use ($response) {
             if ($exception instanceof HttpException) {
                 return $response->error($exception->getMessage(), $exception->getStatusCode());
             }
@@ -54,7 +56,7 @@ return Application::configure(basePath: dirname(__DIR__))
                     'exception' => $exception::class,
                     'file' => $exception->getFile(),
                     'line' => $exception->getLine(),
-                    'trace' => collect($exception->getTrace())->map(fn ($trace) => Arr::except($trace, ['args']))->all(),
+                    // 'trace' => collect($exception->getTrace())->map(fn ($trace) => Arr::except($trace, ['args']))->all(),
                 ];
 
                 return $response->error($exception->getMessage(), data: $data);
--- config/cors.php
+++ config/cors.php
@@ -27,7 +27,7 @@ return [
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 60 * 60 * 12,
 
     'supports_credentials' => false,
 
